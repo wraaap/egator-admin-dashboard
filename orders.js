9fbc4cec8b412e546a17e172071c0caaf97573ae
Maria Sidneva <88075630+wraaap@users.noreply.github.com>
2023-06-26 13:30:40 +0300

    prod

---
+++ orders.js
@@ -0,0 +1,80 @@
+const Orders = [
+    {
+        productName: 'Ruko Fu Pro Drone',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Drone with Camera',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Powerful Racing Drone',
+        productNumber: '12345',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Professional Aerial Drone',
+        productNumber: '67890',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable GPS Drone',
+        productNumber: '24680',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Nano Quadcopter Drone',
+        productNumber: '13579',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Camera Drone with Follow Me Mode',
+        productNumber: '86420',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Mini Pocket Drone',
+        productNumber: '97531',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable FPV Drone',
+        productNumber: '80246',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Stunt Drone with 360° Flips',
+        productNumber: '71458',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with 4K HD Camera',
+        productNumber: '36924',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Quadcopter Drone with Altitude Hold',
+        productNumber: '58017',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+];
